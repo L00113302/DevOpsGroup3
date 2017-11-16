922b42aaf34ead8c368d6607aa870bd8d290a5d0
ckinnego <32260387+ckinnego@users.noreply.github.com>
2017-11-16 23:23:24 +0000

    LoginV1

---
+++ L00119571/src/LogInTester.java
@@ -0,0 +1,19 @@
+/**
+ * Log in tester
+ *
+ * @author Christopher Doherty
+ */
+
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.util.*;
+
+public class LogInTester {
+
+	
+	public static void main(String[] args) 
+	{
+		LogInMenu logInTest = new LogInMenu();
+	}
+}
